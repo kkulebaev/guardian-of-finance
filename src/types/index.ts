2555d20cbfc06b6530e11fef4a19c050318cde05
kkulebaev <konstantinkulebaev@gmail.com>
2022-12-30 00:23:30 +0300

    add some types

---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+export enum Categories {
+  'rent' = 'rent',
+  'clothes' = 'clothes',
+  'supermarket' = 'supermarket',
+  'transport' = 'transport',
+  'other' = 'other',
+  'subscription' = 'subscription',
+  'home' = 'home',
+  'gift' = 'gift',
+  'beauty' = 'beauty',
+  'health' = 'health',
+  'entertainment' = 'entertainment',
+  'travel' = 'travel',
+  'expensive' = 'expensive',
+  'help' = 'help',
+}
+
+export type ICategory = {
+  id: Categories
+  label: string
+}
+
+export interface IUser {
+  id: number
+  name: string
+}
+
+export interface IOperation {
+  userId: number
+  month: string
+  category: Categories
+  sum: number
+}
