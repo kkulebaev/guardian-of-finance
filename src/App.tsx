75f91d256b2b716211987885a8e725eb64974861
kkulebaev <konstantinkulebaev@gmail.com>
2023-01-16 23:34:32 +0300

    add NAV_ITEMS const

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,10 @@
 import './App.css'
-import { Layout, Menu, MenuProps } from 'antd'
+import { Layout, Menu } from 'antd'
 import React, { useState } from 'react'
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons'
 import Main from './pages/Main'
+import { NAV_ITEMS } from './helpers'
 
 const { Header, Content, Footer, Sider } = Layout
-
-type MenuItem = Required<MenuProps>['items'][number]
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem
-}
-
-const items: MenuItem[] = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
-    getItem('Tom', '3'),
-    getItem('Bill', '4'),
-    getItem('Alex', '5'),
-  ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [
-    getItem('Team 1', '6'),
-    getItem('Team 2', '8'),
-  ]),
-  getItem('Files', '9', <FileOutlined />),
-]
 
 function App() {
   const [collapsed, setCollapsed] = useState(false)
@@ -64,7 +27,7 @@ function App() {
           theme="dark"
           defaultSelectedKeys={['1']}
           mode="inline"
-          items={items}
+          items={NAV_ITEMS}
         />
       </Sider>
       <Layout>
