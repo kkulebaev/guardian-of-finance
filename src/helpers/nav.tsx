9f66e427b34bdaff665201f1a6285133ec6e0c50
kkulebaev <konstantinkulebaev@gmail.com>
2023-01-17 10:52:40 +0300

    add routing

--- src/helpers/nav.tsx
+++ src/helpers/nav.tsx
@@ -1,8 +1,19 @@
-import { DesktopOutlined } from '@ant-design/icons'
+import { BarChartOutlined, DollarCircleOutlined } from '@ant-design/icons'
 import { MenuProps } from 'antd'
+import { NavLink } from 'react-router-dom'
 
 type MenuItem = Required<MenuProps>['items'][number]
 
+// TODO: Типизировать роуты, чтобы label нельзя было ошибиться в NavLink to
 export const NAV_ITEMS: MenuItem[] = [
-  { label: 'Расходы', key: '1', icon: <DesktopOutlined /> },
+  {
+    label: <NavLink to="/"> Дашбоард </NavLink>,
+    key: '1',
+    icon: <BarChartOutlined />,
+  },
+  {
+    label: <NavLink to="/costs"> Расходы </NavLink>,
+    key: '2',
+    icon: <DollarCircleOutlined />,
+  },
 ]
