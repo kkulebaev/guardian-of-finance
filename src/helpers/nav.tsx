b87f3e454a23ddefb6f587db1ae53291129054ba
kkulebaev <konstantinkulebaev@gmail.com>
2023-04-18 07:47:55 +0300

    feat: add logout

--- src/helpers/nav.tsx
+++ src/helpers/nav.tsx
@@ -8,21 +8,30 @@ import { NavLink } from 'react-router-dom'
 
 type MenuItem = Required<MenuProps>['items'][number]
 
+export const enum NAV {
+  dashboard = 'Dashboard',
+  costs = 'Costs',
+  logout = 'Logout',
+}
+
 // TODO: Типизировать роуты, чтобы label нельзя было ошибиться в NavLink to
 export const NAV_ITEMS: MenuItem[] = [
   {
+    title: NAV.dashboard,
     label: <NavLink to="/"> Dashboard </NavLink>,
-    key: '1',
+    key: NAV.dashboard,
     icon: <BarChartOutlined />,
   },
   {
+    title: NAV.costs,
     label: <NavLink to="/costs"> Costs </NavLink>,
-    key: '2',
+    key: NAV.costs,
     icon: <DollarCircleOutlined />,
   },
   {
-    label: <NavLink to="/login"> Logout </NavLink>,
-    key: '3',
+    title: NAV.logout,
+    label: NAV.logout,
+    key: NAV.logout,
     icon: <LeftOutlined />,
   },
 ]
