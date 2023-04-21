d2d47b7385da3b46a781ff905a8df0c2bf9bb45b
kkulebaev <konstantinkulebaev@gmail.com>
2023-04-21 18:21:50 +0300

    build: add react-query-devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClientProvider } from 'react-query'
 import { RouterProvider } from 'react-router-dom'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { router } from './router/router'
 import { queryClient } from './api/query-instance'
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
