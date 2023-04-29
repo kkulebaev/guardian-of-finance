62d2056bfe0d091490c3159775953bf91890ea1d
kkulebaev <konstantinkulebaev@gmail.com>
2023-04-29 12:09:14 +0300

    feat: create user info description on profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Spin } from 'antd'
 import ProfileAvatar from '../components/ProfileAvatar'
+import { useGetUser } from '../api/services/profile/use-get-user'
+import ProfileDescription from '../components/ProfileDescription'
 
 function Profile() {
-  return <ProfileAvatar />
-}
+  const { data: user, isLoading } = useGetUser()
 
+  if (isLoading)
+    return <Spin className="flex justify-center items-center h-full" />
+
+  return (
+    <>
+      <ProfileAvatar />
+      <ProfileDescription user={user} />
+    </>
+  )
+}
 export default Profile
